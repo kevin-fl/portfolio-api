9aa29cb10693a6d5314451992c919c6df7aeefcf
kevin-fl <97680218+kevin-fl@users.noreply.github.com>
2022-05-08 14:24:27 +0200

    Update commentaire-router.js

--- routes/commentaire-router.js
+++ routes/commentaire-router.js
@@ -1,5 +1,5 @@
 const commentaireController = require('../controllers/commentaire-controller');
-const { authentificateJwt } = require('../middlewares/authentificate-jwt');
+const { authentificateJwt } = require('../middlewares/authentificate-jwt');    // pour authentifier le token utiliser avec le password du user 
 const bodyValidation = require('../middlewares/body-validation-middleware');
 const commentaire = require('../models/commentaire');
 const { commentaireValidator } = require('../validators/commentaire-validator');
@@ -7,8 +7,9 @@ const { commentaireValidator } = require('../validators/commentaire-validator');
 
 const commentaireRouter = require('express').Router();
 
+//creation des routes pour les methodes get (recup l infos) , post envoi une nouvelle info (add) 
 commentaireRouter.route('/')
-    .get(commentaireController.getAll)
+    .get(commentaireController.getAll)     // autorise le controller a utiliser les differentes methodes .get , .post etc 
     .post(authentificateJwt({ adminRight: true }), bodyValidation(commentaireValidator), commentaireController.add);
 
 commentaireRouter.route('/:id([0-9]+)')
@@ -18,4 +19,4 @@ commentaireRouter.route('/:id([0-9]+)')
 module.exports = commentaireRouter;
 
 
-    // ln 11 .update voir si correcte 
+    
