5c99e7c2af1941c2acf5abbe96c04b04e194ca48
kevin-fl <97680218+kevin-fl@users.noreply.github.com>
2022-05-08 13:57:37 +0200

    Update commentaire.js

--- models/commentaire.js
+++ models/commentaire.js
@@ -2,8 +2,10 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
 
+//     utilisation de sequelize (orm) permet la connexion a une base de donnée (sql )
     const Commentaire = sequelize.define('commentaire', {
 
+//         on defini les champs de la db , commentaire prendra un id de type number et un content de type string 
         id: {
             type: DataTypes.BIGINT,
             autoIncrement: true,
@@ -23,4 +25,4 @@ module.exports = (sequelize) => {
 
 
 
-// y a t'il besoin d avoir Member_Id , Project_Id et Commentaires_Id ?
+/
